d4196ac2afb18635b66789a79da8bfc441640735
Gabriel Gomes <soares2032@gmail.com>
2022-08-13 22:52:57 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -12,4 +12,27 @@ public class Board {
 		pieces = new Piece[rows][colums];
 	}
 
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColums() {
+		return colums;
+	}
+
+	public void setColums(int colums) {
+		this.colums = colums;
+	}
+
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 }
